2352b03e67a32d71e1906ec4ab1a517ba85ece26
Will <wp1199960@gmail.com>
2025-05-05 13:29:38 -0500

    took away resizing font bc it looked weird

--- GUI.py
+++ GUI.py
@@ -1,7 +1,6 @@
 import tkinter as tk
-import tkinter.font as tkFont
 import os
-import final_skeletal_code.py as fsc
+## import final_skeletal_code.py as fsc
 
 ## Function that waters plant
 def waterPlant():
@@ -12,7 +11,7 @@ def waterPlant():
         print(f"Watered {plant}!")
     else:
         print("Please select a plant first.")
-    fsc.water_plant()
+    ## fsc.water_plant()
     ### Recieve input from soil moisture sensor ###
 
 ## Function that adds plant to the database
@@ -65,40 +64,35 @@ root.geometry("300x330")
 root.configure(bg="light green")
 
 ## Project Title Label
-titleFont  = tkFont.Font(family='Arial', size=16)
-titleLabel = tk.Label(root, text="Plant Watering System", font=titleFont,\
-                      bg="light green")
+titleLabel = tk.Label(root, text="Plant Watering System",\
+                      font=('Arial', 20),bg="light green")
 titleLabel.pack(pady=20, expand=True, fill='both')
 
 ## Manual Water Button
-waterButtonFont = tkFont.Font(family='Arial', size=14)
 waterButton     = tk.Button(root, text="Water Plant", command=waterPlant,\
-                        font=waterButtonFont, bg="#ffffff", fg="#444444",\
-                        padx=20, pady=10)
+                        font=('Arial', 14), bg="#ffffff",\
+                            fg="#444444", padx=20, pady=10)
 waterButton.pack(pady=20, expand=True, fill='both')
 
 ## Times Manually Watered Label
 timesWatered = 0
-timesFont    = tkFont.Font(family='Arial', size=14)
 timesLabel   = tk.Label(root, text=f"Times Watered: {timesWatered}",\
-                        font=timesFont, bg="light green")
+                        font=('Arial',14), bg="light green")
 timesLabel.pack(pady=5, expand=True, fill='both')
 
 ## Dropdown Menu
 setDisp        = tk.StringVar()
 setDisp.set('Select Plant')
 displayOptions = ['Select Plant']
-dropDownFont   = tkFont.Font(family='Arial', size=14)
 dropDown       = tk.OptionMenu(root, setDisp, *displayOptions)
-dropDown.config(font=dropDownFont)
+dropDown.config(font=('Arial',14))
 dropDown.pack(pady=5, expand=True, fill='both')
 plantWaterCounts = {}
 loadData()
 
 ## Plant Entry Label
-entryLabelFont  = tkFont.Font(family='Arial', size=14)
 plantEntryLabel = tk.Label(root, text="Enter new plant name:",\
-                           font=entryLabelFont, bg="light green")
+                           font=('Arial',14), bg="light green")
 plantEntryLabel.pack(pady=5, expand=True, fill='both')
 
 ## Frame
@@ -106,14 +100,12 @@ newPlantFrame = tk.Frame(root, bg='light green')
 newPlantFrame.pack(expand=True, fill='both')
 
 ## Plant Entry Button
-entryButtonFont = tkFont.Font(family='Arial', size=14)
-plantEntry      = tk.Entry(newPlantFrame, font=entryButtonFont)
+plantEntry      = tk.Entry(newPlantFrame, font=('Arial',14))
 plantEntry.pack(side=tk.LEFT, padx=5, expand=True, fill='both')
 
 ## Add Plant Button
-addPlantFont   = tkFont.Font(family='Arial', size=14)
 addPlantButton = tk.Button(newPlantFrame, text='Add Plant', command=addPlant,\
-                           font=addPlantFont, bg='#ffffff', fg='#444444',\
+                           font=('Arial',14), bg='#ffffff', fg='#444444',\
                            padx=20, pady=10)
 addPlantButton.pack(side=tk.LEFT, padx=5, expand=True, fill='both')
 
@@ -122,20 +114,6 @@ def onClose():
     saveData()
     root.destroy()
 
-root.protocol("WM_DELETE_WINDOW", onClose)
-
-## Function that changes font size
-def resize_fonts(event):
-    newSize = max(8, int(event.width / 25))
-    titleFont.configure(size=newSize)
-    waterButtonFont.configure(size=newSize)
-    timesFont.configure(size=newSize)
-    dropDownFont.configure(size=newSize)
-    entryLabelFont.configure(size=newSize)
-    entryButtonFont.configure(size=newSize)
-    addPlantFont.configure(size=newSize)
-    
-
-root.bind("<Configure>", resize_fonts)
+root.protocol("WM_DELETE_WINDOW", onClose)    
 
 root.mainloop()
